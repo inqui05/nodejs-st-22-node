1c7b3de44ed36bf2d99d50dc28f43ab146b133d2
Artsiom Tratsiuk <inqui05@gmail.com>
2022-07-13 15:06:25 +0300

    fix: moved duplicate lines to function

--- src/task-2-1.js
+++ src/task-2-1.js
@@ -1,20 +1,19 @@
 const { stdin, stdout } = require('process');
 stdin.setEncoding('utf8');
 
-const reverseString = (string) => {
-  return string.trim().split('').reverse().join('');
+const reverseAndSendToOutputStream = (string) => {
+  const result = string.trim().split('').reverse().join('');
+  stdout.write(`${result}\n`);
 }
 
 const reverseStdin = () => {
   let argsString = process.argv.slice(2).join(' ');
-  stdout.write(`${reverseString(argsString)}\n`);
+  reverseAndSendToOutputStream(argsString);
 
   stdin.on('readable', () => {
     let chunk;
     while ((chunk = stdin.read()) !== null) {
-      const result = reverseString(chunk);
-
-      stdout.write(`${result}\n`);
+      reverseAndSendToOutputStream(chunk);
     }
   });
 
