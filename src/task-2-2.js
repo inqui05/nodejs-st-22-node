d0705709c55f97cc06f7224f0ac9228149a751f5
Artsiom Tratsiuk <inqui05@gmail.com>
2022-07-15 13:26:26 +0300

    feat: solve second task

---
+++ src/task-2-2.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const readline = require('readline');
+const csv = require('csvtojson');
+
+const file = './src/nodejs-hw1-ex1.csv';
+const newFile = './src/nodejs-hw1.ex1.txt';
+
+const rStream = fs.createReadStream(file, {encoding: 'utf-8'});
+const wStream = fs.createWriteStream(newFile, {encoding: 'utf-8'});
+
+const rl = readline.createInterface({
+  input: csv().fromStream(rStream),
+  output: wStream
+});
+
+const loggingErrors = (err) => {
+  console.error(err);
+}
+
+rl.on('line', (line) => {
+  wStream.write(`${line}\n`);
+});
+
+rStream.on('error', loggingErrors);
+wStream.on('error', loggingErrors);
